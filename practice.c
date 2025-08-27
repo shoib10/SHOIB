0602416be844dcef39d15680637995866a8f5571
shoib10 <shoibk1010@gmail.com>
2025-08-27 20:04:38 +0530

    Create practice.c
    100 day practice of c

---
+++ practice.c
@@ -0,0 +1,13 @@
+// 1:  Write a program to input two numbers and display their sum
+
+#include<stdio.h>
+int main () {
+    int a, b;
+    printf("enter a : ");
+    scanf("%d", &a);
+    printf("enter b : ");
+    scanf("%d", &b);
+    
+    printf("sum is %d\n", a+b);
+
+}
